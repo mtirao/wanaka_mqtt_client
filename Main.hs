50ec4dd81a4d3b074f1ba61fc5c6f5e8761f1bd1
Marcos Tirao <marcos.tirao@icloud.com>
2025-09-26 19:05:46 +0200

    Fix queue

--- Main.hs
+++ Main.hs
@@ -5,42 +5,62 @@ import Web.Scotty
 import Network.Wai.Middleware.RequestLogger (logStdoutDev, logStdout)
 import Data.Text (Text, unpack, pack)
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
-import Data.Aeson (Value)
+import Data.Aeson (Value, fromJSON, encode)
+import Data.UUID.V4 (nextRandom)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TLE
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.Configurator as C
 import qualified Data.Configurator.Types as C
+import Data.Time.Clock.POSIX (getPOSIXTime, utcTimeToPOSIXSeconds)
+import Data.Time.Clock (getCurrentTime)
 import Network.MQTT.Client
+import Network.MQTT.Topic (unTopic)
 import Network.URI (parseURI)
 import Network.HTTP.Client
-
-
+import MessageModel (MessageModel(..), MessageType(..))
+import Control.Monad.Trans.Class (MonadTrans(lift))
+import GHC.Int
 
 bsLazyToText :: BL.ByteString -> [Char]
 bsLazyToText = TL.unpack . TLE.decodeUtf8 
 
-makeRequest :: IO ()
-makeRequest = do
+makeRequest :: MessageModel -> IO ()
+makeRequest msgModel = do
     manager <- newManager defaultManagerSettings
-    let request = "http://localhost:8080/api/profile"
+    initialRequest <- parseRequest "http://localhost:3000/api/wanaka/message"
+    let request = initialRequest {
+            method = "POST",
+            requestBody = RequestBodyLBS (encode msgModel),
+            requestHeaders = [("Content-Type", "application/json"), ("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTc2MDYyMjAsInVzZXIiOiIzYmQyMzlkNC0wM2QxLTQ3Y2UtYWUyMC1kYjE1YjIzOTgwMTEifQ.NMx9rryCuxFzDKNxBymI4fVYNJKSwHiM3IEdcm4c2a4")]
+        }
     response <- httpLbs request manager
     putStrLn $ "Response status: " ++ show (responseStatus response)
 
 
+
 msgReceived :: MQTTClient -> Topic -> BL.ByteString -> [Property] -> IO ()
 msgReceived _ topic message property = do
-    makeRequest
-    putStrLn $ "Message: " ++ bsLazyToText message
+    let topicName = unTopic topic  -- Convert ByteString to String
+    currentTime <- getCurrentTime
+    let posixTime = round (utcTimeToPOSIXSeconds currentTime) :: Int64
+    uuid <- nextRandom
+    case topicName of
+      "alert" -> makeRequest (MessageModel (decodeUtf8 $ BL.toStrict message) posixTime Alert (Just uuid))
+      "info" -> makeRequest (MessageModel (decodeUtf8 $ BL.toStrict message) posixTime Info (Just uuid))
+      "error" -> makeRequest (MessageModel (decodeUtf8 $ BL.toStrict message) posixTime Fail (Just uuid))
+      "warn" -> makeRequest (MessageModel (decodeUtf8 $ BL.toStrict message) posixTime Warn (Just uuid))
+
+
+  --  makeRequest
+  --  putStrLn $ "Message: " ++ bsLazyToText message
 
 main :: IO ()
 main = do
   let (Just uri) = parseURI "mqtt://192.168.0.56"
   mc <- connectURI mqttConfig{_msgCB=SimpleCallback msgReceived} uri
-  print =<< subscribe mc [("tmp/topic1", subOptions), ("tmp/topic2", subOptions)] []
+  print =<< subscribe mc [("alert", subOptions), ("error", subOptions), ("info", subOptions), ("warn", subOptions)] []
   waitForClient mc   -- wait for the the client to disconnect
 
- -- where
- --   msgReceived _ t m p = print (t,m,p)
 
---
+++ src/models/MessageModel.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# language DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+
+module MessageModel where
+
+import Data.Text 
+import Data.Aeson
+import GHC.Int
+import Data.UUID (UUID)
+import GHC.Generics (Generic)
+
+data MessageType = Info | Fail | Warn | Alert
+    deriving (Eq, Enum, Bounded, Generic)
+    deriving stock (Read, Show)
+
+
+instance FromJSON MessageType where
+    parseJSON = withText "MessageType" $ \t -> case t of
+        "Info"  -> pure Info
+        "Error" -> pure Fail
+        "Warn"  -> pure Warn
+        "Alert" -> pure Alert
+        _       -> fail $ "Unknown MessageType: " ++ show t
+
+instance ToJSON MessageType where
+    toJSON Info  = String "Info"
+    toJSON Fail = String "Error"
+    toJSON Warn  = String "Warn"
+    toJSON Alert = String "Alert"
+
+-- Message Request
+data MessageModel = MessageModel
+    { messageContent :: Text
+    ,  messageDate :: Int64
+    ,  messageType :: MessageType
+    , messageId :: Maybe UUID
+    } deriving (Show)
+
+instance FromJSON MessageModel where
+    parseJSON (Object v) = MessageModel <$>
+        v .: "messageContent" <*>
+        v .: "messageDate" <*>
+        v .: "messageType" <*>
+        v .:? "messageId"
+
+instance ToJSON MessageModel where
+    toJSON (MessageModel {..}) = object
+        [ "messageId" .= messageId
+        , "messageContent" .= messageContent
+        , "messageDate" .= messageDate
+        , "messageType" .= messageType
+        ]
+
+fromMessageType :: MessageType -> Text     
+fromMessageType Info  = "info"
+fromMessageType Fail = "error"
+fromMessageType Warn  = "warn"
+fromMessageType Alert = "alert"
+
+toMessageType :: Text -> Maybe MessageType
+toMessageType "info"  = Just Info
+toMessageType "error" = Just Fail
+toMessageType "warn"  = Just Warn
+toMessageType "alert" = Just Alert
+toMessageType _       = Nothing
